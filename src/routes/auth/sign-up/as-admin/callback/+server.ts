089d991fb68cca489abd6550f23ce1931cea6f70
tnraro <deokji53@gmail.com>
2024-03-11 20:44:43 +0900

    feat: split admin sign-up page

--- src/routes/auth/sign-up/as-admin/callback/+server.ts
+++ src/routes/auth/sign-up/as-admin/callback/+server.ts
@@ -1,6 +1,7 @@
 import { createAdmin, currentUserExists } from "$edgedb/queries";
 import { NAME } from "$lib/shared/schema/auth.js";
 import { error, redirect } from "@sveltejs/kit";
+import { EdgeDBError } from "edgedb";
 
 export const GET = async ({ locals, url }) => {
   const session = locals.auth.session;
@@ -12,9 +13,19 @@ export const GET = async ({ locals, url }) => {
   }
   const name = NAME.parse(url.searchParams.get("name"));
 
-  await createAdmin(session.client, {
-    name,
-  });
+  try {
+    await createAdmin(session.client, {
+      name,
+    });
+  } catch (error) {
+    if (error instanceof EdgeDBError) {
+      if (error.name === "ConstraintViolationError") {
+        await createAdmin(session.client, {
+          name: name + Math.random(),
+        });
+      }
+    }
+  }
 
   redirect(303, "/");
 };
