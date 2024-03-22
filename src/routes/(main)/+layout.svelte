0d9532ffabf2d6f206388ac801a32cb905db7d52
tnraro <deokji53@gmail.com>
2024-03-22 13:44:37 +0900

    fix: handle unsigned user

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -6,26 +6,43 @@
   <a href="/">4월의 낙원호</a>
   <div class="user">
     {#if data.isSignedIn}
-      <span class="tokens">
-        <enhanced:img class="tokens_img" src="$img/token.png?w=24" />
-        {0}
-      </span>
-      <span class="chips">
-        <enhanced:img class="chips_img" src="$img/chip.png?w=24" />
-        {0}
-      </span>
+      <div class="money">
+        <span class="tokens">
+          <enhanced:img class="tokens_img" src="$img/token.png?w=24" />
+          {0}
+        </span>
+        <span class="chips">
+          <enhanced:img class="chips_img" src="$img/chip.png?w=24" />
+          {0}
+        </span>
+      </div>
       <span>러너 이름</span>
+    {:else}
+      <a class="sign-in" href="/auth/sign-in">로그인</a>
     {/if}
   </div>
 </header>
+
 {@render children()}
 
 <style lang="scss">
   .header {
     display: flex;
     justify-content: space-between;
+    padding: 0.25rem 2rem;
+    align-items: center;
+    background: white;
+    border-bottom: 2px solid var(--slate-3);
+    margin-bottom: 5rem;
   }
   .user {
+    display: flex;
+    gap: 1rem;
+    background: var(--slate-2);
+    padding: 0.25rem 0.5rem;
+    border-radius: 0.25rem;
+  }
+  .money {
     display: flex;
     gap: 0.5rem;
   }
@@ -46,5 +63,8 @@
       width: auto;
       height: 1.5rem;
     }
+  }
+  .sign-in {
+    text-decoration: none;
   }
 </style>
