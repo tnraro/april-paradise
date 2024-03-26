8ad5380b7dcb97c02f7bd182ac4b017f43bc8dea
Hojin Yang <deokji53@gmail.com>
2024-03-27 01:07:14 +0900

    feat: add guard to events (#48)
    close #46

---
+++ src/lib/data/sheets/utils.ts
@@ -0,0 +1,10 @@
+import { getScheduleData } from "./sheets";
+
+export const isScheduleActived = async (key: string) => {
+  const scheduleData = await getScheduleData();
+  const schedule = scheduleData.find((x) => key === x.key);
+  if (schedule == null) return false;
+  const now = new Date();
+  const isActived = now >= schedule.start && now <= schedule.end;
+  return isActived;
+};
--- src/routes/(main)/events/cocktail/+page.server.ts
+++ src/routes/(main)/events/cocktail/+page.server.ts
@@ -1,17 +1,12 @@
-import { getRouletteData } from "$lib/data/sheets/sheets";
 import { isScheduleActived } from "$lib/data/sheets/utils";
 import { redirect } from "@sveltejs/kit";
 
 export const load = async ({ locals }) => {
-  if (!(await isScheduleActived("룰렛"))) {
+  if (!(await isScheduleActived("칵테일"))) {
     redirect(303, "/");
   }
   if (!(await locals.auth.session.isSignedIn())) {
     redirect(303, "/auth/sign-in");
   }
-
-  const rouletteData = await getRouletteData();
-  return {
-    rouletteData,
-  };
+  return {};
 };
