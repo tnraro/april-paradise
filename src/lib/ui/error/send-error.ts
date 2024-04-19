2c76c264563e49169959f92f73a760d8dfe8f477
tnraro <deokji53@gmail.com>
2024-04-19 12:06:29 +0900

    feat: use sentry in send-error

--- src/lib/ui/error/send-error.ts
+++ src/lib/ui/error/send-error.ts
@@ -1,4 +1,11 @@
+import * as Sentry from "@sentry/browser";
+
 export const sendError = (message: string, additionalInformation?: unknown) => {
+  Sentry.captureException(
+    new Error(message, {
+      cause: additionalInformation,
+    }),
+  );
   navigator.sendBeacon(
     "/api/errors",
     JSON.stringify({ message, additionalInformation }),
